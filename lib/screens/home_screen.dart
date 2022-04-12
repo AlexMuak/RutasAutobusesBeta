0951b1c38e207c7a169e2e888ea5bae80d8f1b22
AlexMuak <103475420+AlexMuak@users.noreply.github.com>
2022-04-11 22:00:32 -0500

    Add files via upload

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int currentPage = 1;
+  final PageController pageController = new PageController( initialPage: 1 );
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+    //* AppBar
+      appBar: AppBar(
+        //title: Text('Hola mary $currentPage'),
+        title: Text('Rutas de Autobuses'),
+        elevation: 0,
+      ),
+
+    //? Cambiar la pantalla
+      body: PageView(
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
+        children: const [
+          CustomScreen(color: Colors.blue),
+          CustomScreen(color: Colors.pink),
+          CustomScreen(color: Colors.indigo),
+        ],
+      ),
+      //body: currentPage == 0
+            //? CustomScreen(color: Colors.pink )
+            //: currentPage == 1
+                //?CustomScreen(color: Colors.indigo)
+                //:CustomScreen(color: Colors.green),
+
+    //* Tabs
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (index) {
+          currentPage = index;
+
+          pageController.animateToPage(index, duration: Duration( milliseconds: 300 ),
+              curve: Curves.easeOut
+          );
+
+          setState(() {});
+        },
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(0.5),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon( Icons.verified_user_outlined ),
+              label: 'User'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon( Icons.settings ),
+              label: 'Settings'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon( Icons.car_rental_outlined ),
+              label: 'Cart'
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class CustomScreen extends StatelessWidget {
+
+  final Color color;
+
+  const CustomScreen({ required this.color });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: const Center(
+        //child: Text('Custom Screen'),
+        child: Text('Rutas'),
+      ),
+    );
+  }
+}
